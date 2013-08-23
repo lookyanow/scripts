d04b060a35fdfccc0d6a64d02a2643a7a7a01204
Ivan Lookyanow <lookyanow@gmail.com>
2013-08-24 03:08:34 +0400

    bottletest.py added

---
+++ bottletest.py
@@ -0,0 +1,18 @@
+
+from bottle import route, run, template
+
+@route('/hello/<name>')
+def index(name='World'):
+    return template('<b>Hi {{name}}</b>!', name=name)
+
+@route('/')
+def index():
+    return template('<b>Test index page</b>')
+
+@route('/test')
+def index():
+    return "This is test string"
+
+
+run(host='localhost', port=8080)
+
